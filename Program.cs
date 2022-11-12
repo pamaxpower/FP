8a3b7554fdc820f9b71cf922d6ecc280a16eff47
lordIK <jetlord@mail.ru>
2022-11-12 15:50:25 +0300

    program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями,
+ лучше обойтись исключительно массивами
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите количество элементов массива");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+string[] Array = new string[elementsCount];
+
+for (int i = 0; i < Array.Length; i++)                                      // Заполняем массив с клавиатуры
+{
+    Console.Write($"Введите элемент массива под индексом {i}:\t");
+    Array[i] = Convert.ToString(Console.ReadLine());
+}
+Console.Clear();
+
+Console.WriteLine($"Заданный массив: {String.Join(",", Array)}");           // Вывод первоначального массив
+string[] newArray = FillNewArray(Array);                                    
+Console.WriteLine($"Полученный массив: {String.Join(" ", newArray)}");      // Вывод нового массива
+
+string[] FillNewArray (string[] arr)                                        // Заполнение нового массива
+{
+    string[] newArray = new string[Array.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) 
+        {
+            newArray[count] = arr[i];
+            count++;
+        }
+    }
+    return newArray;
+}
